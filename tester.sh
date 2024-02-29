a0db10382473e2933867e33ecddc7c725f617110
Maximilien Bernede <bernede.maximilien@gmail.com>
2024-02-29 13:50:42 +0100

    added

--- tester.sh
+++ tester.sh
@@ -7,6 +7,21 @@ NC='\033[0m'
 
 TIMEOUT=0.5
 
+run_test_print() {
+    ./cub3D "$1" > /dev/null 2> /dev/null &
+    #./cub3D "$1" &
+    command_pid=$!
+    sleep $TIMEOUT
+
+    if ps -p $command_pid > /dev/null; then
+        kill -TERM $command_pid > /dev/null 2>&1
+        wait $command_pid 2>/dev/null
+        return 1
+    fi
+    wait $command_pid 2>/dev/null
+    return 0
+}
+
 run_test() {
     ./cub3D "$1" > /dev/null 2> /dev/null &
     #./cub3D "$1" &
@@ -41,6 +56,7 @@ print_result() {
 run_tests() {
     local path=$1
     local label=$2
+    local print=$3
 
     echo -e "${BLUE}START $label TESTS${NC}"
     for file in $path/*.cub
@@ -50,6 +66,16 @@ run_tests() {
     done
 }
 
+chmod 766 textures/no_rights.png
+chmod 766 maps/bad/forbidden.cub
+
+
+echo -e "Do you want to see your prints from cub3d?:"
+echo -e "(it can help to see Sefgaults or free errors)"
+echo -e "1. Yes"
+echo -e "2. No"
+read -p "Enter your choice (1/2): " print
+
 echo -e "Choose an option:"
 echo -e "1. Check all files"
 echo -e "2. Check only bad files"
@@ -58,14 +84,14 @@ read -p "Enter your choice (1/2/3): " choice
 
 case $choice in
     1)
-        run_tests "maps/bad" "BAD"
-        run_tests "maps/good" "GOOD"
+        run_tests "maps/bad" "BAD" print
+        run_tests "maps/good" "GOOD" print
         ;;
     2)
-        run_tests "maps/bad" "BAD"
+        run_tests "maps/bad" "BAD" print
         ;;
     3)
-        run_tests "maps/good" "GOOD"
+        run_tests "maps/good" "GOOD" print
         ;;
     *)
         echo -e "${RED}Invalid choice${NC}"
